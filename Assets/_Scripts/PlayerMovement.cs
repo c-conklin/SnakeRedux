f493829ed0eb0fbb0a7138e272858786152a2cdd
clintcc <clintcconklin@gmail.com>
2024-09-29 22:13:58 -0600

    Enhance player movement with speed multiplier
    Added a speed multiplier to adjust player movement speed dynamically. Additionally, modified the scene background color to a darker grey tone. These changes improve visual contrast and fine-tune player control.

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -6,29 +6,35 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Header("Player Movement")]
     [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float speedMultiplier = 2.0f;
     
     private Vector2 direction;
     
     
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) direction = Vector2Int.up;
-        
-        if (Input.GetKey(KeyCode.S)) direction = Vector2Int.down;
-        
-        if (Input.GetKey(KeyCode.A)) direction = Vector2Int.left;
-        
-        if (Input.GetKey(KeyCode.D)) direction = Vector2Int.right;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction = Vector2Int.up;
+        }
 
-        // make players transform translate to rounded int values
-        transform.Translate(direction * speed * Time.deltaTime);
-    }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction = Vector2Int.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction = Vector2Int.left;
+        }
 
-    private void FixedUpdate()
-    {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction = Vector2Int.right;
+        }
+
+        transform.Translate(direction * (speed * speedMultiplier) * Time.deltaTime);
     }
 }
